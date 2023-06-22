b2c3f0fa58c9ad5e81b312f727ee11267aacaa2e
HoangCongMin <0782708546hoangcongminh@gmail.com>
2023-06-22 22:27:57 +0700

    okkkkk

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,29 @@ import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
 import { Inter } from "@next/font/google";
+import Logo3i from "../public/images/logo/logo.png";
+import {  ResolvingMetadata  ,type Metadata} from 'next'
+
 
 const inter = Inter({ subsets: ["latin"] });
+
+
+export const metadata = {
+  title: "Information - Intelliegent - Innovation",
+  description: "3i được sáng lập với lý tưởng vận dụng sức sáng tạo của con người về măt công nghệ để phục vụ xã hội những sản phẩm và giải pháp mang tính trí tuệ và hiệu quả cao trong cuộc sống.",
+};
+
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> { 
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    openGraph: {
+      images: [Logo3i.src, ...previousImages],
+    },
+  }
+}
 
 export default function Home() {
   return (
